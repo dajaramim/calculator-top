9bcf254ca97d1b67e7a1bca4c466f91670fc98a3
Daniel Jaramillo Moya <da.jaramim@gmail.com>
2023-12-21 14:38:17 -0300

    feat: math functions added

---
+++ app.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(a, operator, b) {
+    if (operator === '+') return add(a, b)
+    if (operator === '-') return substract(a, b)
+    if (operator === '*') return multiply(a, b)
+    if (operator === '/') return divide(a, b)
+}
